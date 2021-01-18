a9777cd6c5ebfc55ef7e29daae2cfa91dce12119
PAyeSH <sj.milad@yahoo.com>
2021-01-18 06:10:59 -0800

    milad toast

--- app/src/main/java/com/github/ideveloperr/gitapp/MainActivity.java
+++ app/src/main/java/com/github/ideveloperr/gitapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.github.ideveloperr.gitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                
+                
     }
 }
